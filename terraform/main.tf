7dceab7877422192c1da77755744079c3b0263c8
kartikesh95 <KARTIKESH.NADAR@MICROLAND.COM>
2022-11-19 20:33:47 +0000

    fixed provider issue

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,3 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "kartazcaftfstates"
-    storage_account_name = "kartazcaftfstate01"
-    container_name       = "kartazcaftfstatedev"
-    key                  = "kartazcaftfstatedev.tfstate"
-  }
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.0"
-    }
-    # features {}
-  }
-}
-
-# provider "azurerm" {
-#   # The "feature" block is required for AzureRM provider 2.x.
-#   # If you're using version 1.x, the "features" block is not allowed.
-#   version = "~>2.0"
-#   features {}
-# }
-
 data "azurerm_client_config" "current" {}
 
 #Create Resource Group
---
+++ terraform/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "kartazcaftfstates"
+    storage_account_name = "kartazcaftfstate01"
+    container_name       = "kartazcaftfstatedev"
+    key                  = "kartazcaftfstatedev.tfstate"
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x.
+  # If you're using version 1.x, the "features" block is not allowed.
+  features {}
+}
